76e85dd73e0478e304caf24a3935f48cc0f7b505
Ihor <igignt@gmail.com>
2019-05-08 21:09:10 +0300

    Removed some code

--- script.py
+++ script.py
@@ -1,17 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greeting(who):
-    greeting = 'Hello, {}'.format(who)
-    return greeting
-
 
 r = requests.get('https://google.com')
 print(r.status_code)
+print(r.ok)
